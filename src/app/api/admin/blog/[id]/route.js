323e28d03f56addb6bd24e8c79bbb09dd38e0120
iftikhar ali <ifti.hazara205@gmail.com>
2025-07-21 11:53:18 +0500

    nonth

--- src/app/api/admin/blog/[id]/route.js
+++ src/app/api/admin/blog/[id]/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/blogconnectdb";
-    await connectDB();
 
 import Blog from "@/model/Blog"; // adjust path based on your setup
 import fs from "fs";

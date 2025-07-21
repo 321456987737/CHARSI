5fb902e382e16a1e7315aeb09df35a487a2b5ba3
iftikhar ali <ifti.hazara205@gmail.com>
2025-07-21 11:39:38 +0500

    seventh and eight

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -3,10 +3,9 @@
 
 import { Suspense } from 'react';
 import SigninForm from '@/componenets/auth/signinform';
-
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading.....</div>}>
       <SigninForm />
     </Suspense>
   );

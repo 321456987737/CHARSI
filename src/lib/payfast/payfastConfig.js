86bf98365c66bf9569530e0dd28b9a541bdff7ff
iftikhar ali <ifti.hazara205@gmail.com>
2025-07-26 19:34:37 +0500

    blalnlaa

--- src/lib/payfast/payfastConfig.js
+++ src/lib/payfast/payfastConfig.js
@@ -1,8 +1,8 @@
 export const payfastConfig = {
   merchant_id: process.env.PAYFAST_MERCHANT_ID,
   merchant_key: process.env.PAYFAST_MERCHANT_KEY,
-  return_url: `${process.env.NEXT_PUBLIC_APP_URL}/api/payfast/return`,
-  cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/payment-cancelled`,
-  notify_url: process.env.PAYFAST_NOTIFY_URL, // Keep this for future production use
+  return_url:process.env.PAYFAST_RETURN_URL,
+  cancel_url: process.env.PAYFAST_CANCEL_URL,
+  notify_url: process.env.PAYFAST_NOTIFY_URL,
   sandboxUrl: "https://sandbox.payfast.co.za/eng/process"
 };

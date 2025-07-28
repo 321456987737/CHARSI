fc54b2db5195e4b68c38f2053f8517f296dfa12a
iftikhar ali <ifti.hazara205@gmail.com>
2025-07-28 18:55:11 +0500

    after adding few perfection part 7

--- src/app/(app)/userdashboard/layout.js
+++ src/app/(app)/userdashboard/layout.js
@@ -4,17 +4,13 @@ import { usePathname } from "next/navigation";
 import Navbar from "@/componenets/main/navbar/page";
 import BlogFooter from "@/componenets/footer/inpagefooter";
 
-
 export default function MainLayout({ children }) {
   const pathname = usePathname();
 
-  // Show footer only on these specific pages
- const showFooter = [
-  "/userdashboard",
-  "/userdashboard/categoryblog",
-  "/userdashboard/readblog",
-].some((path) => pathname?.startsWith(path));
-
+  const showFooter =
+    pathname === "/userdashboard" ||
+    pathname.startsWith("/userdashboard/categoryblog") ||
+    pathname.startsWith("/userdashboard/readblog");
 
   return (
     <>

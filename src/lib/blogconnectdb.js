4c4c618417156cc741bbdd96d9e83c909874eb34
iftikhar ali <ifti.hazara205@gmail.com>
2026-04-16 10:38:12 +0500

    Revert "lovely1"
    This reverts commit 5aa86e7e44bbcf0486e2c730d01872bb6f7db502.

--- src/lib/blogconnectdb.js
+++ src/lib/blogconnectdb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI_BLOG;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variables");
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 let cached = global.mongoose;

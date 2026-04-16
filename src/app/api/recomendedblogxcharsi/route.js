00ab90b971b6a940f5240769a492190af3f2c00b
iftikhar ali <ifti.hazara205@gmail.com>
2026-04-16 10:38:09 +0500

    Revert "mamaia"
    This reverts commit 0fe6c6b60ea3689af63a66f815932d948964a715.

--- src/app/api/recomendedblogxcharsi/route.js
+++ src/app/api/recomendedblogxcharsi/route.js
@@ -1,23 +1,21 @@
+
 import { connectDB } from "@/lib/blogconnectdb";
 import Blog from "@/model/Blog";
 import { NextResponse } from "next/server";
-export async function GET() {
-  try {
-    console.log(1);
-    await connectDB();
-    console.log("2");
-    const blogs = await Blog.find({})
-      .sort({ views: -1 }) // Sort by views, highest first
-      .limit(6)
-      //   .populate("author", "username profileImage") // ✅ populate only specific fields
-      .lean();
-    console.log(2);
+export async function GET(){
+   try{
+      console.log(1)
+      await connectDB();
+      console.log("2")
+   const blogs = await Blog.find({})
+  .sort({ views: -1 }) // Sort by views, highest first
+  .limit(6)
+//   .populate("author", "username profileImage") // ✅ populate only specific fields
+  .lean();
+      console.log(2)
 
-    return NextResponse.json({ success: true, blogs });
-  } catch (err) {
-    return NextResponse.json(
-      { error: "Failed to fetch blogs", success: false },
-      { status: 500 },
-    );
-  }
+   return NextResponse.json({ success: true, blogs });
+}catch(err){
+   return NextResponse.json({ error: "Failed to fetch blogs", success: false }, { status: 500 });
+}
 }
